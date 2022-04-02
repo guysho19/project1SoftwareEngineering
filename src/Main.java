6bf16830b18c31c7ffca3c27a5c9619a1b61c83a
guysho19 <guy.shoef@campus.technion.ac.il>
2022-04-02 18:11:41 +0300

    Add getIndexes()

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,12 @@ public class Main {
     public static Scanner scanner;  // Note: Do not change this line.
     public static final int VALID = 1;
     public static final int INVALID = 0;
-    public static final char ACADEMICALLY_VALID_SIGN='▮';
-    public static final char ACADEMICALLY_INVALID_SIGN='▯';
+    public static final char ACADEMICALLY_VALID_SIGN ='▮';
+    public static final char ACADEMICALLY_INVALID_SIGN ='▯';
+    public static final String YOKRA = "Yokra";
+    public static final int SUB_INPUT_LENGTH = 2;
+
+
 
     public static void theStudentsGame() {
         System.out.println("Dear president, please enter the board’s size.");
@@ -15,20 +19,14 @@ public class Main {
         int rows=scanner.nextInt();
         scanner.next();
         int columns=scanner.nextInt();
+        scanner.nextLine();
         int[][] students_board=new int[rows][columns];
         initializeBoard(students_board);
-        System.out.println("Dear president, please enter the cell's indexes.");
+        //printBoard(students_board);
+        getIndexes(students_board);
     }
     
-    public static void printBoard (int[][] board, int numOfRows, int numOfColumns){
-        for (int i=0;i<numOfRows;i++){
-            for(int j=0;j<numOfColumns;j++){
-                if (board[i][j]==1) System.out.print(ACADEMICALLY_VALID_SIGN);
-                else System.out.print(ACADEMICALLY_INVALID_SIGN);
-            }
-            System.out.println();
-        }
-    }
+
     
     public static void initializeBoard(int[][] students_board) {
         for (int i = 0; i <students_board.length ; i++) {
@@ -37,15 +35,49 @@ public class Main {
             }
         }
     }
-    public static void printBoard (int[][] board, int numOfRows, int numOfColumns){
-        for (int i=0;i<numOfRows;i++){
-            for(int j=0;j<numOfColumns;j++){
-                if (board[i][j]==1) System.out.print(ACADEMICALLY_VALID_SIGN);
+    public static void getIndexes(int[][] students_board) {
+        String input = "";
+        int row=0,column=0;
+        while(!input.equals(YOKRA)){
+            System.out.println("Dear president, please enter the cell's indexes.");
+            input=scanner.nextLine();
+             String[] subInput=input.split(", ",SUB_INPUT_LENGTH);
+             if (subInput.length==SUB_INPUT_LENGTH){
+                 row =Integer.parseInt(subInput[0]);
+                 column =Integer.parseInt(subInput[1]);
+                if (isInBoard(students_board,row,column)){
+                    if (students_board[row][column]==INVALID){
+                        students_board[row][column]=VALID;
+                    }else{
+                        students_board[row][column]=INVALID;
+                    }
+                }else{
+                    System.out.println("The cell is not within the board’s boundaries, enter a new cell.");
+                }
+             }
+            //printBoard(students_board);
+        }
+       // printBoard(students_board);
+
+    }
+    public static boolean isInBoard(int[][] students_board,int row, int column) {
+        if (row < 0 || row >= students_board.length || column < 0 || column >= students_board[0].length)
+             return false;
+        return true;
+    }
+
+
+        public static void printBoard (int[][] board){
+        for (int i=0;i< board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if (board[i][j]==1)
+                    System.out.print(ACADEMICALLY_VALID_SIGN);
                 else System.out.print(ACADEMICALLY_INVALID_SIGN);
             }
             System.out.println();
         }
     }
+
 
     public static void main(String[] args) throws IOException {
         //String path = args[0];

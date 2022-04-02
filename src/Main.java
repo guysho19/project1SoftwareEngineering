e957faad6b7491ad8d482e4fc90d8ee3ef8fd944
kfir7755 <102858028+kfir7755@users.noreply.github.com>
2022-04-02 16:28:13 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class Main {
     public static Scanner scanner;  // Note: Do not change this line.
     public static final int VALID = 1;
-    public static final int NOT_VALID = 0;
+    public static final int INVALID = 0;
+    public static final char ACADEMICALLY_VALID_SIGN='▮';
+    public static final char ACADEMICALLY_INVALID_SIGN='▯';
 
     public static void theStudentsGame() {
         System.out.println("Dear president, please enter the board’s size.");
@@ -15,6 +17,17 @@ public class Main {
         int[][] students_board=new int[rows][columns];
         System.out.println("Dear president, please enter the cell's indexes.");
     }
+    
+    public static void printBoard (int[][] board, int numOfRows, int numOfColumns){
+        for (int i=0;i<numOfRows;i++){
+            for(int j=0;j<numOfColumns;j++){
+                if (board[i][j]==1) System.out.print(ACADEMICALLY_VALID_SIGN);
+                else System.out.print(ACADEMICALLY_INVALID_SIGN);
+            }
+            System.out.println();
+        }
+    }
+    
     public static void initializeBoard(int[][] students_board) {
         for (int i = 0; i <students_board.length ; i++) {
             for (int j = 0; j <students_board[0].length ; j++) {
